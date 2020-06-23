3555f79c648776525957f43fa1089f91fffbd7ef
Chima <64258165+cugbo@users.noreply.github.com>
2020-06-23 10:15:25 +0100

    Add files via upload

---
+++ leaderboard.php
@@ -0,0 +1,27 @@
+
+<!doctype html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=5ef0f923b61c220011098161&product=inline-share-buttons" async="async"></script>
+
+  <title> LeaderBoard</title>
+    <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Paaji+2&display=swap" rel="stylesheet">
+    <link href="leaderboard.css" rel="stylesheet">
+  
+</head>
+
+<body>
+<span><p> Welcome to the HNGi7 Leaderboard</span><br>
+<div class="sharethis-inline-share-buttons"></div>
+
+<center><table id='table' border cellpadding=3>
+       <tr><th>Fullname</th><th>Username</th><th>Email</th></th><th>Score</th>
+       </tr></table></center>
+
+<script type="text/javascript" src="jquery-3.2.1.js"></script>
+<script type="text/javascript" src="leaderboard.js"></script>
+
+
+</body>
+</html>
